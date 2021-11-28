e9144195985d04a5972474fa9c083b6f921b9574
orpitad <95119856+orpitad@users.noreply.github.com>
2021-11-28 15:31:16 +0530

    Sum of all numbers in an array

---
+++ sum.js
@@ -0,0 +1,9 @@
+function sumOfNumbers(arrayOfNumbers){
+  let sum = 0;
+  for(var i=0; i< arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i];
+  }
+  return sum;
+}
+
+console.log(sumOfNumbers([1,5,6]);
